2adb1848d41cd9afef8cbad49ebd31326b0291b5
maebee-cm <zee_cm@protonmail.com>
2023-10-18 05:54:07 -0600

    add upload data and rocket dep

--- florust_common/src/lib.rs
+++ florust_common/src/lib.rs
@@ -1,5 +1,12 @@
+use rocket::FromForm;
+
 pub mod server_plugin;
 pub mod server_data_source_error;
+
+#[derive(FromForm)]
+pub struct UploadedData {
+    pub data: Vec<u8>
+}
 
 #[cfg(test)]
 mod tests {
