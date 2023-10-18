7f8fbf004af046a0be9fc5879a39671cb9a38205
Nahla <nahla.nee@protonmail.com>
2023-10-17 23:46:46 -0600

    change supertrait constrain to be Send + Sync. Change class to data_source_type. Make register, unregister, and upload_data return Result with error of DataSourceError

--- florust_common/src/plugin.rs
+++ florust_common/src/plugin.rs
@@ -1,25 +1,27 @@
 use async_trait::async_trait;
 
+use crate::server_data_source_error::DataSourceError;
+
 #[async_trait]
-pub trait AtomicDataSourceManager {
-    fn class(&self) -> &'static str;
+pub trait AtomicDataSourceManager: Sync + Send {
+    fn data_source_type(&self) -> &'static str;
 
-    async fn register(&self, id: String);
+    async fn register(&mut self, id: String) -> Result<(), DataSourceError>;
 
-    async fn unregister(&self, id: &str);
+    async fn unregister(&mut self, id: &str) -> Result<(), DataSourceError>;
 
-    async fn update_data(&self, id: &str);
+    async fn update_data(&self, id: &str) -> Result<(), DataSourceError>;
 }
 
 #[async_trait]
-pub trait DataSourceManager {
-    fn class(&self) -> &'static str;
+pub trait DataSourceManager: Sync + Send {
+    fn data_source_type(&self) -> &'static str;
 
-    async fn register(&self, id: String);
+    async fn register(&mut self, id: String) -> Result<(), DataSourceError>;
 
-    async fn unregister(&self, id: &str);
+    async fn unregister(&mut self, id: &str) -> Result<(), DataSourceError>;
 
-    async fn update_data(&mut self, id: &str);
+    async fn update_data(&mut self, id: &str) -> Result<(), DataSourceError>;
 }
 
 pub type CreateAtomicDataSourceManager = unsafe extern fn() -> Box<Box<dyn AtomicDataSourceManager>>;
