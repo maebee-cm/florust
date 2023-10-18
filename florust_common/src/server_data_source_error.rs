ee8d5a3e7befbde4f9e64eec54f48079916076b2
Nahla <nahla.nee@protonmail.com>
2023-10-17 23:45:31 -0600

    updated with more errors and added thiserror

--- florust_common/src/server_data_source_error.rs
+++ florust_common/src/server_data_source_error.rs
@@ -1,6 +1,12 @@
 use serde::{Serialize, Deserialize};
+use thiserror::Error;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Error, Debug)]
 pub enum DataSourceError {
+    #[error("Attempted to register data source ID, but ID already exists in given data source type manager.")]
     IdAlreadyExists,
+    #[error("Attempted to access data source ID, but ID doesn't exist in given data source type manager.")]
+    IdDoesntExist,
+    #[error("Attempted to interact with data source type, but manager doesn't exist")]
+    DataSourceTypeDoesntExist
 }
