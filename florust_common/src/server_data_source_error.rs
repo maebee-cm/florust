848e6fcbc8d77b541ee6ea8b2b3ab04bba5c042a
maebee-cm <zee_cm@protonmail.com>
2023-11-03 00:39:54 -0600

    rustfmt changes

--- florust_common/src/server_data_source_error.rs
+++ florust_common/src/server_data_source_error.rs
@@ -1,12 +1,16 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
 #[derive(Serialize, Deserialize, Error, Debug)]
 pub enum DataSourceManagerError {
-    #[error("Attempted to register data source ID, but ID already exists in given data source manager.")]
+    #[error(
+        "Attempted to register data source ID, but ID already exists in given data source manager."
+    )]
     IdAlreadyExists,
-    #[error("Attempted to access data source ID, but ID doesn't exist in given data source manager.")]
+    #[error(
+        "Attempted to access data source ID, but ID doesn't exist in given data source manager."
+    )]
     IdDoesntExist,
     #[error("Attempted to interact with data source manager, but manager doesn't exist")]
-    DataSourceManagerDoesntExist
+    DataSourceManagerDoesntExist,
 }
