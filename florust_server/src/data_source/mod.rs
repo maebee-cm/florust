1e46fed1b60f4376927d137314558a39e4f1d631
Nahla <nahla.nee@protonmail.com>
2023-11-05 03:14:48 -0700

    changed to fit new changed to plugin format

--- florust_server/src/data_source/mod.rs
+++ florust_server/src/data_source/mod.rs
@@ -1,41 +1,85 @@
-use florust_common::{server_data_source_error::DataSourceManagerError, server_plugin::DataSourceManager, UploadedData};
-use rocket::{post, put, State, Responder, form::Form};
+use florust_common::{
+    server_data_source_error::DataSourceManagerError,
+    UploadedData
+};
+use rocket::{form::Form, post, put, Responder, State};
 
 use crate::FlorustState;
 
 #[derive(Responder)]
-#[response(status = 404, content_type = "json")]
-pub struct NotFoundErrorResponder(String);
+pub enum DataSourceError {
+    #[response(status = 404, content_type = "json")]
+    NotFound(String),
+    #[response(status = 409, content_type = "json")]
+    Conflict(String)
+}
+
+impl From<DataSourceManagerError> for DataSourceError {
+    fn from(value: DataSourceManagerError) -> Self {
+        match value {
+            DataSourceManagerError::IdAlreadyExists => Self::Conflict(
+                serde_json::to_string(&value)
+                    .expect("Failed to serialize valid DataSourceManagerError")
+            ),
+            DataSourceManagerError::IdDoesntExist | DataSourceManagerError::DataSourceManagerDoesntExist => {
+                Self::NotFound(serde_json::to_string(&value)
+                    .expect("Failed to serialize valid DataSoucreManagerError")
+                )
+            },
+        }
+    }
+}
 
 #[derive(Responder)]
 #[response(status = 200)]
-pub struct OkResponder(());
+pub struct OkResponder<T>(T) where T: Send + Sync;
 
-fn manager_exists_or_err<'a>(state: &'a State<FlorustState>, manager_id: &str) -> Result<&'a Box<dyn DataSourceManager>, NotFoundErrorResponder> {
-    state.get_manager(manager_id).ok_or(NotFoundErrorResponder(
-        serde_json::to_string(&DataSourceManagerError::DataSourceManagerDoesntExist)
-            .expect("Failed to serialize valid DataSourceError::DataSourceTypeDoesntExist")
-    ))
+#[post("/register/<manager_id>/<data_source_id>", data = "<data>")]
+pub async fn register(
+    state: &State<FlorustState>,
+    manager_id: String,
+    data_source_id: String,
+    data: Option<Form<UploadedData>>
+) -> Result<OkResponder<()>, DataSourceError> {
+    if let Some(data) = data {
+        state.register_data_source(&manager_id, data_source_id, Some(data.data.as_slice())).await
+            .map(OkResponder)
+            .map_err(DataSourceError::from)
+    }
+    else {
+        state.register_data_source(&manager_id, data_source_id, None).await
+            .map(OkResponder)
+            .map_err(DataSourceError::from)
+    }
 }
 
-fn manager_op_success_or_err(manager_id: &str, result: Result<(), DataSourceManagerError>) -> Result<OkResponder, NotFoundErrorResponder>{
-    result.map(OkResponder).map_err(|err| NotFoundErrorResponder(
-        serde_json::to_string(&err)
-            .expect(&format!("Data source manager (name: {}) returned unserializable error!", manager_id))
-    ))
+#[post("/unregister/<manager_id>/<data_source_id>", data = "<data>")]
+pub async fn unregister(
+    state: &State<FlorustState>,
+    manager_id: String,
+    data_source_id: String,
+    data: Option<Form<UploadedData>>
+) -> Result<OkResponder<()>, DataSourceError> {
+    if let Some(data) = data {
+        state.deregister_data_source(&manager_id, &data_source_id, Some(data.data.as_slice())).await
+            .map(OkResponder)
+            .map_err(DataSourceError::from)
+    }
+    else {
+        state.deregister_data_source(&manager_id, &data_source_id, None).await
+            .map(OkResponder)
+            .map_err(DataSourceError::from)
+    }
 }
 
-#[post("/register/<manager_id>/<id>")]
-pub async fn register(state: &State<FlorustState>, manager_id: String, id: String) -> Result<OkResponder, NotFoundErrorResponder> {
-    manager_op_success_or_err(&manager_id, manager_exists_or_err(state, &manager_id)?.register(id).await)
-}
-
-#[post("/unregister/<manager_id>/<id>")]
-pub async fn unregister(state: &State<FlorustState>, manager_id: String, id: String) -> Result<OkResponder, NotFoundErrorResponder> {
-    manager_op_success_or_err(&manager_id, manager_exists_or_err(state, &manager_id)?.unregister(&id).await)
-}
-
-#[put("/upload_data/<manager_id>/<id>", data="<data>")]
-pub async fn upload_data(state: &State<FlorustState>, manager_id: String, id: String, data: Form<UploadedData>) -> Result<OkResponder, NotFoundErrorResponder> {
-    manager_op_success_or_err(&manager_id, manager_exists_or_err(state, &manager_id)?.update_data(&id, &data.data).await)
+#[put("/upload_data/<manager_id>/<data_source_id>", data = "<data>")]
+pub async fn upload_data(
+    state: &State<FlorustState>,
+    manager_id: String,
+    data_source_id: String,
+    data: Form<UploadedData>,
+) -> Result<OkResponder<()>, DataSourceError> {
+    state.update_data(&manager_id, &data_source_id, data.data.as_slice()).await
+        .map(OkResponder)
+        .map_err(DataSourceError::from)
 }
