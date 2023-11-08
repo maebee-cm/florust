37a3f2b9b04386443b2f96c09c7aac786f5519b9
maebee-cm <zee_cm@protonmail.com>
2023-11-07 19:46:58 -0700

    move to new error system and add getter for data

--- florust_server/src/manager_and_data.rs
+++ florust_server/src/manager_and_data.rs
@@ -1,23 +1,29 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, result};
 
-use florust_common::{server_plugin::{IIntegerDataSourceManager, UIntegerDataSourceManager, FloatDataSourceManager, self}, server_data_source_error::DataSourceManagerError};
+use florust_common::{server_plugin::{IIntegerDataSourceManager, UIntegerDataSourceManager, FloatDataSourceManager}, FlorustServerPluginError};
 use rocket::{async_trait, tokio::sync::RwLock};
+use thiserror::Error;
 
 use crate::circular_vec::CircularVec;
 
 enum DataSourceStatus<T> where T: Send + Sync {
+    Registered(CircularVec<T>),
     RegisteredNoData,
-    RegisteredWithData(CircularVec<T>),
-    Deregistered
+    Deregistered(CircularVec<T>)
 }
 
 impl<T> DataSourceStatus<T> where T: Send + Sync {
-    fn is_deregistered(&self) -> bool {
-        if let DataSourceStatus::Deregistered = self {
-            true
+    fn is_registered(&self) -> bool {
+        match self {
+            Self::Registered(_) | Self::RegisteredNoData => true,
+            Self::Deregistered(_) => false
         }
-        else {
-            false
+    }
+
+    fn data_or_err<O: FnOnce() -> ManagerAndDataError>(&self, op: O) -> Result<&CircularVec<T>> {
+        match self {
+            Self::Registered(data) | Self::Deregistered(data) => Ok(data),
+            Self::RegisteredNoData => Err(op())
         }
     }
 }
@@ -33,17 +39,37 @@ type UIntegerLoggedData = LoggedData<u64>;
 type FloatDataManager = Box<dyn FloatDataSourceManager>;
 type FloatLoggedData = LoggedData<f64>;
 
+enum DataType {
+    IInteger(i64),
+    UInteger(u64),
+    Float(f64)
+}
+
+#[derive(Error, Debug)]
+enum ManagerAndDataError {
+    #[error("Data source manager returned error: {0}")]
+    DataSourceManager(FlorustServerPluginError),
+    #[error("Attempted to access data from a data source but it has no reported data")]
+    NoData,
+    #[error("Attempted to access data from a data source but an out of bounds index was used")]
+    IndexOutOfBounds
+}
+
+type Result<T> = result::Result<T, ManagerAndDataError>;
+
 #[async_trait]
 pub trait ManagerAndData: Send + Sync {
-    async fn register(&self, id: String) -> server_plugin::Result<()>;
+    async fn register(&self, id: String) -> Result<()>;
 
-    async fn register_with_data(&self, id: String, data: &[u8]) -> server_plugin::Result<()>;
+    async fn register_with_data(&self, id: String, data: &[u8]) -> Result<()>;
 
-    async fn deregister(&self, id: &str) -> server_plugin::Result<()>;
+    async fn deregister(&self, id: &str) -> Result<()>;
 
-    async fn deregister_with_data(&self, id: &str, data: &[u8]) -> server_plugin::Result<()>;
+    async fn deregister_with_data(&self, id: &str, data: &[u8]) -> Result<()>;
 
-    async fn update_data(&self, id: &str, data: &[u8]) -> server_plugin::Result<()>;
+    async fn update_data(&self, id: &str, data: &[u8]) -> Result<()>;
+
+    async fn get_data(&self, id: &str, index: usize) -> Result<DataType>;
 }
 
 pub struct IIntegerManagerAndData {
@@ -65,7 +91,7 @@ pub struct FloatManagerAndData {
 }
 
 macro_rules! manager_and_data_impl {
-    ($impl_for:ident, $data_manager:ty) => {
+    ($impl_for:ident, $data_manager:ty, $data_type:path) => {
         impl $impl_for {
             pub fn new(manager: $data_manager, max_logged_data_size: usize) -> $impl_for {
                 $impl_for {
@@ -78,114 +104,204 @@ macro_rules! manager_and_data_impl {
 
         #[async_trait]
         impl ManagerAndData for $impl_for {
-            async fn register(&self, id: String) -> server_plugin::Result<()> {
-                let mut lock = self.logged_data.write().await;
+            async fn register(&self, id: String) -> Result<()> {
+                let lock = self.logged_data.write().await;
+                match lock.get(&id) {
+                    Some(data_source) => {
+                        let data_source = data_source.write().await;
 
-                if let Some(data_source) = lock.get(&id) {
-                    let mut data_source = data_source.write().await;
-                    if !data_source.is_deregistered() {
-                        return Err(DataSourceManagerError::IdAlreadyExists);
+                        if data_source.is_registered() {
+                            return Err(
+                                ManagerAndDataError::DataSourceManager(
+                                    FlorustServerPluginError::DataSourceAlreadyExists(id)
+                                )
+                            )
+                        }
+
+                        self.manager.register(id).await.map_err(|err| {
+                            ManagerAndDataError::DataSourceManager(
+                                FlorustServerPluginError::DataSourceManager(err)
+                            )
+                        })?;
+                        *data_source = DataSourceStatus::RegisteredNoData;
                     }
-
-                    self.manager.register(id).await?;
-                    *data_source = DataSourceStatus::RegisteredNoData;
-                }
-                else {
-                    self.manager.register(id.clone()).await?;
-                    lock.insert(id, RwLock::new(DataSourceStatus::RegisteredNoData));
-                }
-
-                Ok(())
-            }
-
-            async fn register_with_data(&self, id: String, data: &[u8]) -> server_plugin::Result<()> {
-                let mut lock = self.logged_data.write().await;
-
-                if let Some(data_source) = lock.get(&id) {
-                    let mut data_source = data_source.write().await;
-                    if !data_source.is_deregistered() {
-                        return Err(DataSourceManagerError::IdAlreadyExists);
+                    None => {
+                        self.manager.register(id).await.map_err(|err| {
+                            ManagerAndDataError::DataSourceManager(
+                                FlorustServerPluginError::DataSourceManager(err)
+                            )
+                        })?;
+                        lock.insert(id, RwLock::new(DataSourceStatus::RegisteredNoData));
                     }
-
-                    self.manager.register_with_data(id, data).await?;
-                    *data_source = DataSourceStatus::RegisteredNoData;
-                }
-                else {
-                    self.manager.register_with_data(id.clone(), data).await?;
-                    lock.insert(id, RwLock::new(DataSourceStatus::RegisteredNoData));
                 }
 
                 Ok(())
             }
 
-            async fn deregister(&self, id: &str) -> server_plugin::Result<()> {
-                let lock = self.logged_data.read().await;
-                
-                let mut data_source = lock
-                    .get(id)
-                    .ok_or(DataSourceManagerError::IdDoesntExist)?
-                    .write()
-                    .await;
+            async fn register_with_data(&self, id: String, data: &[u8]) -> Result<()> {
+                let lock = self.logged_data.write().await;
+                match lock.get(&id) {
+                    Some(data_source) => {
+                        let data_source = data_source.write().await;
 
-                if data_source.is_deregistered() {
-                    return Err(DataSourceManagerError::IdDoesntExist);
+                        if data_source.is_registered() {
+                            return Err(
+                                ManagerAndDataError::DataSourceManager(
+                                    FlorustServerPluginError::DataSourceAlreadyExists(id)
+                                )
+                            )
+                        }
+
+                        self.manager.register_with_data(id, data).await.map_err(|err| {
+                            ManagerAndDataError::DataSourceManager(
+                                FlorustServerPluginError::DataSourceManager(err)
+                            )
+                        })?;
+                        *data_source = DataSourceStatus::RegisteredNoData;
+                    }
+                    None => {
+                        self.manager.register_with_data(id, data).await.map_err(|err| {
+                            ManagerAndDataError::DataSourceManager(
+                                FlorustServerPluginError::DataSourceManager(err)
+                            )
+                        })?;
+                        lock.insert(id, RwLock::new(DataSourceStatus::RegisteredNoData));
+                    }
                 }
-
-                self.manager.deregister(id).await?;
-                *data_source = DataSourceStatus::Deregistered;
 
                 Ok(())
             }
 
-            async fn deregister_with_data(&self, id: &str, data: &[u8]) -> server_plugin::Result<()> {
-                let lock = self.logged_data.read().await;
-                
-                let mut data_source = lock
+            async fn deregister(&self, id: &str) -> Result<()> {
+                let lock = self.logged_data.write().await;
+                let status = lock
                     .get(id)
-                    .ok_or(DataSourceManagerError::IdDoesntExist)?
-                    .write()
-                    .await;
+                    .ok_or(
+                        ManagerAndDataError::DataSourceManager(
+                            FlorustServerPluginError::DataSourceManagerDoesntExist(id.to_string())
+                        )
+                    )?
+                    .write().await;
 
-                if data_source.is_deregistered() {
-                    return Err(DataSourceManagerError::IdDoesntExist);
+                if !status.is_registered() {
+                    return Err(
+                        ManagerAndDataError::DataSourceManager(
+                            FlorustServerPluginError::DataSourceAlreadyDeregistered(id.to_string())
+                        )
+                    );
                 }
 
-                self.manager.deregister_with_data(id, data).await?;
-                *data_source = DataSourceStatus::Deregistered;
+                self.manager.deregister(&id).await
+                    .map_err(|err| {
+                        ManagerAndDataError::DataSourceManager(
+                            FlorustServerPluginError::DataSourceManager(err)
+                        )
+                    })?;
+
+                match *status {
+                    DataSourceStatus::Registered(data) => *status = DataSourceStatus::Deregistered(data),
+                    DataSourceStatus::RegisteredNoData => {
+                        lock.remove(id);
+                    },
+                    DataSourceStatus::Deregistered(_) => unreachable!("DataSourceStatus is Deregistered despite check saying it isn't.")
+                };
 
                 Ok(())
             }
 
-            async fn update_data(&self, id: &str, data: &[u8]) -> server_plugin::Result<()> {
+            async fn deregister_with_data(&self, id: &str, data: &[u8]) -> Result<()> {
+                let lock = self.logged_data.write().await;
+                let status = lock
+                    .get(id)
+                    .ok_or(
+                        ManagerAndDataError::DataSourceManager(
+                            FlorustServerPluginError::DataSourceManagerDoesntExist(id.to_string())
+                        )
+                    )?
+                    .write().await;
+
+                if !status.is_registered() {
+                    return Err(
+                        ManagerAndDataError::DataSourceManager(
+                            FlorustServerPluginError::DataSourceAlreadyDeregistered(id.to_string())
+                        )
+                    );
+                }
+
+                self.manager.deregister_with_data(&id, data).await
+                    .map_err(|err| {
+                        ManagerAndDataError::DataSourceManager(
+                            FlorustServerPluginError::DataSourceManager(err)
+                        )
+                    })?;
+
+                match *status {
+                    DataSourceStatus::Registered(data) => *status = DataSourceStatus::Deregistered(data),
+                    DataSourceStatus::RegisteredNoData => {
+                        lock.remove(id);
+                    },
+                    DataSourceStatus::Deregistered(_) => unreachable!("DataSourceStatus is Deregistered despite check saying it isn't.")
+                };
+
+                Ok(())
+            }
+
+            async fn update_data(&self, id: &str , data: &[u8]) -> Result<()> {
                 let lock = self.logged_data.read().await;
 
                 let mut data_source = lock
                     .get(id)
-                    .ok_or(DataSourceManagerError::IdDoesntExist)?
+                    .ok_or(
+                        ManagerAndDataError::DataSourceManager(
+                            FlorustServerPluginError::DataSourceDoesntExist(id.to_string())
+                        )
+                    )?
                     .write()
                     .await;
 
-                let val = self.manager.update_data(id, data).await?;
+                let val = self.manager.update_data(id, data).await.map_err(|e| {
+                    ManagerAndDataError::DataSourceManager(
+                        FlorustServerPluginError::DataSourceManager(e)
+                    )
+                })?;
 
                 match &mut *data_source {
                     DataSourceStatus::RegisteredNoData => {
                         let mut logged_data = CircularVec::new(self.max_logged_data_size);
                         logged_data.append(val);
-                        *data_source = DataSourceStatus::RegisteredWithData(logged_data);
+                        *data_source = DataSourceStatus::Registered(logged_data);
                     },
-                    DataSourceStatus::RegisteredWithData(logged_data) => {
+                    DataSourceStatus::Registered(logged_data) => {
                         logged_data.append(val);
-
                     },
-                    DataSourceStatus::Deregistered => return Err(DataSourceManagerError::IdDoesntExist),
+                    DataSourceStatus::Deregistered(_) => return Err(
+                        ManagerAndDataError::DataSourceManager(
+                            FlorustServerPluginError::DataSourceDoesntExist(id.to_string())
+                        )
+                    ),
                 }
 
                 Ok(())
+            }
+
+            async fn get_data(&self, id: &str, index: usize) -> Result<DataType> {
+                Ok($data_type(*self.logged_data.read().await
+                    .get(id)
+                    .ok_or(
+                        ManagerAndDataError::DataSourceManager(
+                            FlorustServerPluginError::DataSourceDoesntExist(id.to_string())
+                        )
+                    )?
+                    .read().await
+                    .data_or_err(|| ManagerAndDataError::NoData)?
+                    .get(index)
+                    .ok_or(ManagerAndDataError::IndexOutOfBounds)?
+                ))
             }
         }
     };
 }
 
-manager_and_data_impl!(IIntegerManagerAndData, IIntegerDataManager);
-manager_and_data_impl!(UIntegerManagerAndData, UIntegerDataManager);
-manager_and_data_impl!(FloatManagerAndData, FloatDataManager);
+manager_and_data_impl!(IIntegerManagerAndData, IIntegerDataManager, DataType::IInteger);
+manager_and_data_impl!(UIntegerManagerAndData, UIntegerDataManager, DataType::UInteger);
+manager_and_data_impl!(FloatManagerAndData, FloatDataManager, DataType::Float);
