676732a83019f44cb9f350e45723c3ef47e64c40
Nahla <nahla.nee@protonmail.com>
2023-11-09 02:10:57 -0700

    add default plugins

---
+++ florust_server/src/default_plugins.rs
@@ -0,0 +1,103 @@
+use std::array::TryFromSliceError;
+
+use florust_common::server_plugin::{DataSourceManager, self, DataSourceManagerError, IIntegerDataSourceManager, UIntegerDataSourceManager, FloatDataSourceManager};
+use rocket::async_trait;
+
+pub struct DefaultIIntegerDataManager;
+
+#[async_trait]
+impl DataSourceManager<i64> for DefaultIIntegerDataManager {
+    fn manager_id(&self) ->  &'static str {
+        "FlorustDefaultIIntegerDataManager"
+    }
+
+    async fn register(&self, _id: String) -> server_plugin::Result<()> {
+        Ok(())
+    }
+
+    async fn register_with_data(&self, _id: String, _data: &[u8]) -> server_plugin::Result<()> {
+        Ok(())
+    }
+
+    async fn deregister(&self, _id: &str) -> server_plugin::Result<()> {
+        Ok(())
+    }
+
+    async fn deregister_with_data(&self, _id: &str, _data: &[u8]) -> server_plugin::Result<()> {
+        Ok(())
+    }
+
+    async fn update_data(&self, _id: &str, data: &[u8]) -> server_plugin::Result<i64> {
+        let data = data.try_into()
+            .map_err(|err:  TryFromSliceError| DataSourceManagerError::InvalidData(err.to_string()))?;
+        Ok(i64::from_be_bytes(data))
+    }
+}
+
+impl IIntegerDataSourceManager for DefaultIIntegerDataManager{}
+
+pub struct DefaultUIntegerDataManager;
+
+#[async_trait]
+impl DataSourceManager<u64> for DefaultUIntegerDataManager {
+    fn manager_id(&self) ->  &'static str {
+        "FlorustDefaultUIntegerDataManager"
+    }
+
+    async fn register(&self, _id: String) -> server_plugin::Result<()> {
+        Ok(())
+    }
+
+    async fn register_with_data(&self, _id: String, _data: &[u8]) -> server_plugin::Result<()> {
+        Ok(())
+    }
+
+    async fn deregister(&self, _id: &str) -> server_plugin::Result<()> {
+        Ok(())
+    }
+
+    async fn deregister_with_data(&self, _id: &str, _data: &[u8]) -> server_plugin::Result<()> {
+        Ok(())
+    }
+
+    async fn update_data(&self, _id: &str, data: &[u8]) -> server_plugin::Result<u64> {
+        let data = data.try_into()
+            .map_err(|err:  TryFromSliceError| DataSourceManagerError::InvalidData(err.to_string()))?;
+        Ok(u64::from_be_bytes(data))
+    }
+}
+
+impl UIntegerDataSourceManager for DefaultUIntegerDataManager{}
+
+pub struct DefaultFloatDataManager;
+
+#[async_trait]
+impl DataSourceManager<f64> for DefaultFloatDataManager {
+    fn manager_id(&self) ->  &'static str {
+        "FlorustDefaultFloatDataManager"
+    }
+
+    async fn register(&self, _id: String) -> server_plugin::Result<()> {
+        Ok(())
+    }
+
+    async fn register_with_data(&self, _id: String, _data: &[u8]) -> server_plugin::Result<()> {
+        Ok(())
+    }
+
+    async fn deregister(&self, _id: &str) -> server_plugin::Result<()> {
+        Ok(())
+    }
+
+    async fn deregister_with_data(&self, _id: &str, _data: &[u8]) -> server_plugin::Result<()> {
+        Ok(())
+    }
+
+    async fn update_data(&self, _id: &str, data: &[u8]) -> server_plugin::Result<f64> {
+        let data = data.try_into()
+            .map_err(|err:  TryFromSliceError| DataSourceManagerError::InvalidData(err.to_string()))?;
+        Ok(f64::from_be_bytes(data))
+    }
+}
+
+impl FloatDataSourceManager for DefaultFloatDataManager{}
