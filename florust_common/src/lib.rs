9e0bb59b1313290f0e8534d520bc3c4d2f760387
maebee-cm <zee.mzha@gmail.com>
2023-11-09 17:04:04 -0700

    organize florust_common by moving server error into server module, and renaming server_plugin.rs to server.rs

--- florust_common/src/lib.rs
+++ florust_common/src/lib.rs
@@ -1,24 +1,8 @@
 use rocket::FromForm;
 
-pub mod server_plugin;
+pub mod server;
 
 use serde::{Serialize, Deserialize};
-use server_plugin::DataSourceManagerError;
-use thiserror::Error;
-
-#[derive(Serialize, Deserialize, Error, Debug)]
-pub enum FlorustServerPluginError {
-    #[error("Attempted to register data source ID ({0}), but it already exists.")]
-    DataSourceAlreadyExists(String),
-    #[error("Attempted to access data source ID ({0}), but ID doesn't exist.")]
-    DataSourceDoesntExist(String),
-    #[error("Attempted to deregister data source ID ({0}), but it already was deregistered")]
-    DataSourceAlreadyDeregistered(String),
-    #[error("Attempted to access data source manager ({0}), but manager doesn't exist")]
-    DataSourceManagerDoesntExist(String),
-    #[error("Data source manager failed with error: {0}")]
-    DataSourceManager(DataSourceManagerError),
-}
 
 
 #[derive(FromForm, Serialize, Deserialize)]
--- florust_common/src/server.rs
+++ florust_common/src/server.rs
@@ -5,6 +5,20 @@ use serde::{Serialize, Deserialize};
 use thiserror::Error;
 
 #[derive(Serialize, Deserialize, Error, Debug)]
+pub enum FlorustServerPluginError {
+    #[error("Attempted to register data source ID ({0}), but it already exists.")]
+    DataSourceAlreadyExists(String),
+    #[error("Attempted to access data source ID ({0}), but ID doesn't exist.")]
+    DataSourceDoesntExist(String),
+    #[error("Attempted to deregister data source ID ({0}), but it already was deregistered")]
+    DataSourceAlreadyDeregistered(String),
+    #[error("Attempted to access data source manager ({0}), but manager doesn't exist")]
+    DataSourceManagerDoesntExist(String),
+    #[error("Data source manager failed with error: {0}")]
+    DataSourceManager(DataSourceManagerError),
+}
+
+#[derive(Serialize, Deserialize, Error, Debug)]
 pub enum DataSourceManagerError {
     #[error("DataSourceManager was given invalid data: {0}")]
     InvalidData(String)
